6bbe9cf74995aa2de641fc391135cc3236c36df6
Alex Sirrosh <35045651+sirrosh@users.noreply.github.com>
2020-05-29 17:04:27 +0200

    Commenting on cycle saving hack, minor cleanup

--- main.asm
+++ main.asm
@@ -1,7 +1,6 @@
 list	p=16f648a
 radix dec
 #include "p16f648a.inc"
-;__config 0x3f78
 __CONFIG   _CP_OFF & _CPD_OFF & _WDT_OFF & _PWRTE_ON & _INTRC_OSC_NOCLKOUT & _MCLRE_OFF & _LVP_OFF
 
 RES_VECT CODE    0x0000          ; processor reset vector
@@ -9,11 +8,11 @@ RES_VECT CODE    0x0000          ; processor reset vector
     banksel TRISB
     CLRF   TRISB                ; port B to output
     banksel PORTB
-    GOTO   0x007D
+    GOTO   0x007D               ; let's start generating!
 
 SINE_TABLE CODE 0x007D          ; using predefined address (the last sine table value) to save one cycle on GOTO
 #include "simple.inc"
-    MOVWF PCL                   ; this will throw us to the beginning, at 0x007D
+    MOVWF PCL                   ; this will throw us to the beginning of the loop, at the 0x007D
 
 
     END
